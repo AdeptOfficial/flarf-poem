17514d805e46ebc9999d57af31910f69eb3d05a0
xAdepTx <48741579+xAdepTx@users.noreply.github.com>
2020-02-06 13:10:02 -0600

    Update redirect2nd.js

--- redirect2nd.js
+++ redirect2nd.js
@@ -1,31 +1,31 @@
 function onClick() {
-  location.replace("2nd-Page.html")
+  location.replace("2nd-Page.html");
 }
 
 function apologizeBtn() {
-  location.replace("3rd-Happy.html")
+  location.replace("3rd-Happy.html");
 }
 
 function angryBtn() {
-  location.replace("3rd-Angry.html")
+  location.replace("3rd-Angry.html");
 }
 
 function badEnd() {
-  location.replace("badEnd.html")
+  location.replace("badEnd.html");
 }
 
 function reTry() {
-  location.replace("index.html")
+  location.replace("index.html");
 }
 
 function violetBtn() {
-  location.replace("4th-Page.html")
+  location.replace("4th-Page.html");
 }
 
 function breathBtn() {
-  location.replace("5th-Page.html")
+  location.replace("5th-Page.html");
 }
 
 function nyanBtn() {
-  location.replace("nyancatpage.html")
+  location.replace("nyancatpage.html");
 }
